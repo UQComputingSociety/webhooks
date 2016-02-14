a396ac093733999bc67a3750603d30a58ea35ec2
Tom Manderson <TRManderson@gmail.com>
2016-02-14 13:37:29 +1000

    Git hooks now work

--- hooks.py
+++ hooks.py
@@ -1,4 +1,7 @@
 from flask import Flask, request
+import os
+import subprocess as sp
+import xmlrpclib
 
 app = Flask(__name__)
 
@@ -7,22 +10,44 @@ services = [
     "slackinv",
     "hubot",
     "website",
+    "hooks",
 ]
 
+with open("template.html") as f:
+    template = f.read()
 
 __all__ = [
     "app",
 ]
 
 
-def git_pull_service_restart(service):
+def supervisor_restart(service):
+    server = xmlrpclib.Server(
+        'http://{username}:{password}@localhost:9001/RPC2'.format(
+                username=os.environ.get("SUPERVISOR_USER","user"),
+                password=os.environ.get("SUPERVISOR_PASS","123"),
+            )
+        )
+
+def git_pull_in_dir(service):
     """
     A service name should match with both its name in supervisor and its /srv/*
     path.
     """
+    previous_cwd = os.getcwd()
+    os.chdir(path)
+    out = sp.check_output(["git", "pull"], timeout=120)
+    out ++ "\n"
+    os.chdir(previous_cwd)
+    return out
+
 
 for service in services:
     def fn():
-        pass
-    
+        return template.format(
+                git_pull_service_restart(service),
+                supervisor_restart(service),
+            )
+    fn.__name__ = serivce + "_update"
+    app.add_url_rule('/'+service, service, fn)
 
