7030f29e8dc602750be32c1551ad7284a7e8665b
Tom Manderson <TRManderson@gmail.com>
2016-02-17 21:20:50 +1000

    Slack notifications that are uselses now happen

--- hooks.py
+++ hooks.py
@@ -2,6 +2,8 @@ from flask import Flask, request
 import os
 import subprocess as sp
 import xmlrpc.client
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -19,6 +21,18 @@ with open("template.html") as f:
 __all__ = [
     "app",
 ]
+
+
+def slack_post(git_pull_result, sueprvisor_result):
+    def format():
+        return "A thing happend, I don't know what."
+    slack_hooks = os.environ.get("SLACK_HOOK_URL")
+    if slack_hooks:
+        requests.post(slack_hooks, json.dumps({
+                "username": "hookbot",
+                "icon_emoji": ":fc:",
+                "text": format()
+            }))
 
 
 def supervisor_restart(service):
@@ -41,21 +55,27 @@ def git_pull_in_dir(service):
     os.chdir("/srv/" + service)
     try:
         out = sp.check_output(["git", "pull"], timeout=120).decode('utf-8')
+        code = 0
     except sp.CalledProcessError as e:
         out = e.output.decode('utf-8')
         out += "\nErrored out with code " + str(e.returncode) + "."
+        code = e.returncode
     out += "\n"
     os.chdir(previous_cwd)
-    return out
+    return out, code
 
 def wrap(service):
     def fn():
-        return template.format(
-                git_pull_in_dir(service),
-                supervisor_restart(service),
+        slack_post(1, 1)
+        git = git_pull_in_dir(service)
+        sup = supervisor_restart(service)
+        msg = template.format(
+                git[0],
+                sup[-1],
             )
+        return msg
     fn.__name__ = service + "_update"
-    return fn 
+    return fn
 
 for service in services:
     app.route('/'+service, methods=["GET", "POST"])(wrap(service))
